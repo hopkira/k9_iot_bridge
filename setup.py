2f5418b0a97fabd084b118416052aa023c8a90f9
Richard Hopkins <hopkira@googlemail.com>
2025-12-02 12:54:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'k9_iot_bridge'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='hopkira',
-    maintainer_email='hopkira@googlemail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='YOUR_NAME',
+    maintainer_email='YOUR_EMAIL@example.com',
+    description='MQTT bridge between Bangle.js joystick and /cmd_vel',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'k9_iot_bridge_node = k9_iot_bridge.k9_iot_bridge_node:main'
+            # this must match your package + module + main()
+            'k9_iot_bridge_node = k9_iot_bridge.k9_iot_bridge_node:main',
         ],
     },
 )
